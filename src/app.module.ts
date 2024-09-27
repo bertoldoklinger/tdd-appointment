feb06299be14338dd5557d58537f79fd7dd78df0
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-09-27 12:02:50 -0300

    chore: add modules to start app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppointmentService } from './appointment/appointment.service';
 import { PatientModule } from './patient/patient.module';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [PatientModule],
+  imports: [PatientModule, AppointmentModule],
   controllers: [],
   providers: [AppointmentService],
 })
