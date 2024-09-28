9e82a792dcec3b63dc4c9ff134fbb5aedb326fd8
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-09-28 13:03:32 -0300

    feat: add http code response to /appointment/schedule and ensure it returns a appointment created

--- src/appointment/http/controller/appointment.controller.ts
+++ src/appointment/http/controller/appointment.controller.ts
@@ -1,9 +1,12 @@
 import {
   Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   InternalServerErrorException,
   Post,
 } from '@nestjs/common';
+import { Appointment } from 'src/appointment/appointment.model';
 import { AppointmentService } from 'src/appointment/appointment.service';
 import { ScheduleAppointmentDTO } from '../dto/schedule-appointment.dto';
 
@@ -12,15 +15,17 @@ export class AppointmentController {
   constructor(private readonly appointmentService: AppointmentService) {}
 
   @Post('schedule')
+  @HttpCode(HttpStatus.CREATED)
   async scheduleAppointment(
     @Body() scheduleAppointmentDTO: ScheduleAppointmentDTO,
-  ): Promise<void> {
+  ): Promise<Appointment> {
     try {
-      await this.appointmentService.scheduleAppointment({
+      const appointment = await this.appointmentService.scheduleAppointment({
         ...scheduleAppointmentDTO,
         startDate: new Date(scheduleAppointmentDTO.startDate),
         endDate: new Date(scheduleAppointmentDTO.endDate),
       });
+      return appointment;
     } catch (error: any) {
       throw new InternalServerErrorException(error.message);
     }
