eb6b75df09930b68d4a6f4b767f735bbdab99974
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-10-02 09:32:03 -0300

    test: add unit tests for patient model

---
+++ src/patient/core/model/patient.model.spec.ts
@@ -0,0 +1,28 @@
+import { PatientModel } from './patient.model';
+
+describe('PatientModel', () => {
+  const DEFAULT_NAME = 'Bertoldo Klinger';
+  const AVERAGE_AGE = 25;
+  const INVALID_AGE = 17;
+
+  it('should create a PatientModel', async () => {
+    const patient = PatientModel.create({
+      name: DEFAULT_NAME,
+      age: AVERAGE_AGE,
+    });
+
+    expect(patient).toEqual({
+      patientId: expect.any(String),
+      name: DEFAULT_NAME,
+      age: AVERAGE_AGE,
+    });
+  });
+  it('should not create a PatientModel with invalid age', async () => {
+    expect(() =>
+      PatientModel.create({
+        name: DEFAULT_NAME,
+        age: INVALID_AGE,
+      }),
+    ).toThrow('patient age must be equal or greather than 18 years');
+  });
+});
