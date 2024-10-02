51470e9b2e19cb8dfff688c4807f868daa280117
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-10-02 08:22:42 -0300

    ensure AppointmentInMemoryRepository confirms a appointment

--- src/appointment/persistence/repository/implementation/in-memory/appointment.in-memory.repository.spec.ts
+++ src/appointment/persistence/repository/implementation/in-memory/appointment.in-memory.repository.spec.ts
@@ -39,4 +39,26 @@ describe('AppointmentInMemoryRepository', () => {
       patientId: expect.any(String),
     });
   });
+  it('should confirm a appointment', async () => {
+    const startDate = new Date('2024-09-27T10:00:00Z');
+    const endDate = new Date('2024-09-27T11:00:00Z');
+    const patientId = randomUUID();
+    const newAppointment = Appointment.create({
+      startDate,
+      endDate,
+      patientId,
+    });
+    await sut.save(newAppointment);
+
+    sut.confirm(newAppointment);
+
+    expect(sut.appointments.length).toBe(1);
+    expect(sut.appointments[0]).toEqual({
+      appointmentId: expect.any(String),
+      startDate: newAppointment.startDate,
+      endDate: newAppointment.endDate,
+      confirmed: true,
+      patientId: expect.any(String),
+    });
+  });
 });
