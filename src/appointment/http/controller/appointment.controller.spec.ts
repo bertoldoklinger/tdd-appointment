58770308058cd2ceabc9811499985f81826d8b67
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-10-02 09:33:41 -0300

    add unit tests for AppointmentController

---
+++ src/appointment/http/controller/appointment.controller.spec.ts
@@ -0,0 +1,70 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { InternalServerErrorException } from '@nestjs/common';
+import { AppointmentService } from 'src/appointment/core/service/appointment.service';
+import { ScheduleAppointmentDTO } from '../dto/schedule-appointment.dto';
+import { AppointmentController } from './appointment.controller';
+
+describe('AppointmentController', () => {
+  let sut: AppointmentController;
+  let appointmentService: DeepMocked<AppointmentService>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppointmentController],
+      providers: [
+        {
+          provide: AppointmentService,
+          useValue: createMock<AppointmentService>(),
+        },
+      ],
+    }).compile();
+    sut = module.get<AppointmentController>(AppointmentController);
+    appointmentService =
+      module.get<DeepMocked<AppointmentService>>(AppointmentService);
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should call appointmentService.scheduleAppointment with correct params', async () => {
+      const input: ScheduleAppointmentDTO = {
+        startDate: new Date(),
+        endDate: new Date(),
+        patientId: 'any_patient_id',
+      };
+
+      await sut.scheduleAppointment(input);
+
+      expect(appointmentService.scheduleAppointment).toHaveBeenCalledWith({
+        startDate: input.startDate,
+        endDate: input.endDate,
+        patientId: input.patientId,
+      });
+      expect(appointmentService.scheduleAppointment).toHaveBeenCalledTimes(1);
+    });
+    it('should throw InternalServerException if appointmentService Throws', async () => {
+      const input: ScheduleAppointmentDTO = {
+        startDate: new Date(),
+        endDate: new Date(),
+        patientId: 'any_patient_id',
+      };
+      appointmentService.scheduleAppointment.mockRejectedValue(
+        new Error("appointment's endTime should be after startTime"),
+      );
+
+      await expect(() => sut.scheduleAppointment(input)).rejects.toThrow(
+        new InternalServerErrorException(
+          "appointment's endTime should be after startTime",
+        ),
+      );
+    });
+  });
+});
